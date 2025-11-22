9d82dc64daa709dcc0d06e7c64f58cea1137752c
invitacionesweb <invitacionesweb.info@gmail.com>
2025-11-22 20:08:36 -0300

    a
    a

--- countdown.js
+++ countdown.js
@@ -30,4 +30,4 @@ const countdown = (deadline, elem, finalMessage ) => {
 };
 
 
-countdown('DEC 07 2025 15:30:51 GMT-0300', 'clock', 'llego el dia')
+countdown('DEC 07 2025 15:00:51 GMT-0300', 'clock', 'llego el dia')
